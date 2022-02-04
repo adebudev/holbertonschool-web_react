1e1cc7146dd43d93c739ff8206ea1368d65d2f3b
AdrianDel07 <delaasuncionbuelvasadrian@gmail.com>
2022-02-03 22:58:02 -0500

    Task:  1. Learning how to use Webpack with a config file

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -3,24 +3,27 @@ const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
+$('body').append('<button id="click-count">Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 const counter = (function () {
-    let counter = 0;
-  
-    return function () {
-      counter++;
-      return counter;
-    };
-  })();
-  
-  function updateCounter() {
-    $('#count').text(counter() + ' clicks on the button');
-  }
-  
-  $('#click-here').on('click', _.debounce(updateCounter, 300, {
+  let counter = 0;
+
+  return function () {
+    counter++;
+    return counter;
+  };
+})();
+
+function updateCounter() {
+  $('#count').text(counter() + ' clicks on the button');
+}
+
+$('#click-count').on(
+  'click',
+  _.debounce(updateCounter, 300, {
     leading: true,
     trailing: false,
-  }));
+  })
+);
